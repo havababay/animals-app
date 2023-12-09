312f0acc3204379956fd3609b94ea6a1a7d48ffa
havababay <hava.babay@gmail.com>
2023-12-09 14:41:40 +0200

    Display Animal component on app root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { AnimalComponent } from './animal/animal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, AnimalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
